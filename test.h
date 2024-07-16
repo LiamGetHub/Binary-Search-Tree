7c86bee3005f027f1456ec4182954fe2883161ae
LiamGetHub <157172169+LiamGetHub@users.noreply.github.com>
2024-07-16 16:15:01 -0700

    Create test.h

---
+++ test.h
@@ -0,0 +1,28 @@
+/*
+Liam Fitting
+CPSC 223
+4/4/23
+*/
+
+#ifndef TESTS_H
+#define TESTS_H
+
+// create a tree with a value
+void testCreateTree();
+
+//you write more tests, at least one test for each operation
+
+void testInsert();
+void testInOrderTest();
+void testFind();
+void testPreOrderTest();
+void testPostOrderTest();
+void testFindMax();
+void testFindMin();
+void testRemove();
+void testSize();
+
+
+
+
+#endif
